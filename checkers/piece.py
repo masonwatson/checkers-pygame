9cc7ad90d0851536dee6b23c036a5cefe3478e24
masonwatson <84687363+masonwatson@users.noreply.github.com>
2021-11-02 13:13:41 -0500

    Board Created

---
+++ checkers/piece.py
@@ -0,0 +1,41 @@
+import pygame
+from .constants import RED, SQUARE_SIZE, WHITE, GREY
+
+class Piece:
+    PADDING = 10
+    OUTLINE = 2
+
+    def __init__(self, row, col, color):
+        self.row = row
+        self.col = col
+        self.color = color
+        self.king = False
+
+        if self.color == RED:
+            self.direction = -1
+        else:
+            self.direction = 1
+
+        self.x = 0
+        self.y = 0
+
+    def calc_pos(self):
+        self.x = SQUARE_SIZE * self.col + SQUARE_SIZE // 2
+        self.y = SQUARE_SIZE * self.row + SQUARE_SIZE // 2
+
+    def make_king(self):
+        self.king = True
+
+    def draw(self, win):
+        radius = SQUARE_SIZE // 2 - self.PADDING
+        pygame.draw.circle(win, GREY, (self.x, self.y), radius + self.OUTLINE)
+        pygame.draw.circle(win, self.color, (self.x, self.y), radius)
+
+    # __repr__ makes debugging easier
+    # we define our own representation of the object
+    def __repr__(self):
+        return str(self.color)
+
+    
+
+    
